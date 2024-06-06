94d618f56bf55269d60c5142eb53c9e6c4b16a55
Chaitanyalonarkar <chaitanyalonarkar@gmail.com>
2024-06-06 23:38:53 +0530

    users-list done

--- src/Users.jsx
+++ src/Users.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { FaUsersGear } from "react-icons/fa6";
 
 export default function Users() {
   const [users, setUsers] = useState([]);
@@ -27,27 +28,56 @@ export default function Users() {
 
   return (
     <>
-      <h1>User</h1>
+      <div className="body d-flex justify-content-center align-items-center ">
+        <div className="dashboard rounded-4 d-flex  ">
+          <div className="left text-white p-3 w-50">
+            <div className="d-flex gap-3 align-items-center">
+                <FaUsersGear className="uicon" />
 
-      <div className="users">
-        {users.map((user) => (
-          //   <ListItem button key={user.id} onClick={() => handleUserClick(user)}>
-          // <ListItemAvatar>
-          //   <Avatar src={`https://robohash.org/${user.id}?set=set2`} />
-          // </ListItemAvatar>
-          // <ListItemText primary={user.name} />
-          //   </ListItem>
-          
-          <div className="users-list">
-
-            <h3>{user.profile.firstName} {user.profile.lastName}</h3>
-            <div>
-              <img src={user.avatar} alt="avatar" />
+              <h3>All Users</h3>
             </div>
-            <p>{user.jobTitle}</p>
+
+            <div className="users-container d-flex flex-column  pointer mt-3 m  ">
+              {loading ? (
+                <div className="spinner">
+                  <div
+                    class="inner spinner-border text-light "
+                    role="status"
+                    style={{ width: "2.5rem", height: "2.5rem" }}
+                  >
+                    <span class="visually-hidden">Loading...</span>
+                  </div>
+                </div>
+              ) : (
+                ""
+              )}
+              {users.map((user, key) => (
+                <div className="user  align-items-center d-flex gap-3 p-2 ps-3 rounded-4">
+                  <div className="pic">
+                    <img
+                      src={`https://randomuser.me/api/portraits/med/men/${key}.jpg`}
+                      alt="avatar"
+                    />
+                    {/* <img src={user.avatar} alt="avatar" /> */}
+                  </div>
+                  <div className="details">
+                    <h6 className="m-0">
+                      {user.profile.firstName} {user.profile.lastName}
+                    </h6>
+                    {/* <div>
+            </div> */}
+                    <p className="m-0">{user.profile.username}</p>
+                    <p className="m-0">{user.jobTitle}</p>
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
-          
-        ))}
+          <div className="right w-50 rounded-3 m-3 p-3 bg-white">
+            <h2>User Information</h2>
+            <p>Click the more nutton to get the user information</p>
+          </div>
+        </div>
       </div>
     </>
   );
